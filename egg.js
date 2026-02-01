61745b0c33a66ca52cc552a104fd83d5ea3763af
not-diba <elvis.madiba@gmail.com>
2026-02-01 19:38:16 +0300

    feat: Functions

--- egg.js
+++ egg.js
@@ -136,11 +136,50 @@ function run(program) {
   return evaluate(parse(program), Object.create(topScope));
 }
 
-run(`
-do(define(total, 0),
-define(count, 1),
-while(<(count, 11),
-do(define(total, +(total, count)),
-define(count, +(count, 1)))),
-print(total))
-`);
+// run(`
+// do(define(total, 0),
+// define(count, 1),
+// while(<(count, 11),
+// do(define(total, +(total, count)),
+// define(count, +(count, 1)))),
+// print(total))
+// `);
+
+specialForms.fun = (args, scope) => {
+  if (!args.length) {
+    throw new SyntaxError("Functions need a body");
+  }
+
+  let body = args[args.length - 1];
+  let params = args.slice(0, args.length - 1).map((expr) => {
+    if (expr.type != "word") {
+      throw new SyntaxError("Parameter names must be words");
+    }
+    return expr.name;
+  });
+
+  return function (...args) {
+    if (args.length != params.length) {
+      throw new TypeError("Wrong number of arguments");
+    }
+
+    let localScope = Object.create(scope);
+    for (let i = 0; i < args.length; i++) {
+      localScope[params[i]] = args[i];
+    }
+    return evaluate(body, localScope);
+  };
+};
+
+// run(`
+// do(define(plusOne, fun(a, +(a, 1))),
+// print(plusOne(10)))
+// `);
+// // → 11
+// run(`
+// do(define(pow, fun(base, exp,
+// if(==(exp, 0),
+// 1,
+// *(base, pow(base, -(exp, 1)))))),
+// print(pow(2, 10)))
+// `);
